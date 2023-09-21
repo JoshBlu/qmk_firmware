a0acd20e8421f947f3ed39b674a65fc06625e495
JoshBlu <>
2023-09-20 20:14:39 -0500

    Fixed typo in comments. Disabled Space Cadet

--- keyboards/dztech/dz65rgb/keymaps/JoshBlu/rules.mk
+++ keyboards/dztech/dz65rgb/keymaps/JoshBlu/rules.mk
@@ -1,5 +1,6 @@
 CAPS_WORD_ENABLE = yes
 VIA_ENABLE = yes
+<<<<<<< HEAD
 LTO_ENABLE = yes	#Reduce the size of the firmware through link time optimization
 #Following rules to reduce size of firmware size
 GRAVE_ESC_ENABLE = no
@@ -16,7 +17,18 @@ CAPS_WORD_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 VIA_ENABLE = yes
 LTO_ENABLE = yes	#Recuse the size of the firmware through link time optimization
+||||||| parent of e2edbbf8b2 (Fuxed typo in comments. Disabled Space Cadet)
+LTO_ENABLE = yes	#Recuse the size of the firmware through link time optimization
+=======
+LTO_ENABLE = yes	#Reduce the size of the firmware through link time optimization
+>>>>>>> e2edbbf8b2 (Fuxed typo in comments. Disabled Space Cadet)
 #Following rules to reduce size of firmware size
 GRAVE_ESC_ENABLE = no
 MUSIC_ENABLE = no
+<<<<<<< HEAD
 >>>>>>> 5a734ba134 (Added via support and reduced firmware size to fit)
+||||||| parent of e2edbbf8b2 (Fuxed typo in comments. Disabled Space Cadet)
+=======
+SPACE_CADET_ENABLE = no
+
+>>>>>>> e2edbbf8b2 (Fuxed typo in comments. Disabled Space Cadet)
