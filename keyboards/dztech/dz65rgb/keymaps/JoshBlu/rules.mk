79cb16c1b1be45ba626e76ea1ef2e8956b418ea9
JoshBlu <>
2023-09-24 10:11:14 -0500

    Manually copy keymap from branch

---
+++ keyboards/dztech/dz65rgb/keymaps/JoshBlu/rules.mk
@@ -0,0 +1,34 @@
+CAPS_WORD_ENABLE = yes
+VIA_ENABLE = yes
+<<<<<<< HEAD
+LTO_ENABLE = yes	#Reduce the size of the firmware through link time optimization
+#Following rules to reduce size of firmware size
+GRAVE_ESC_ENABLE = no
+MUSIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+
+||||||| parent of 483aee036c (Updated Keymap, Enabled Caps Word and defined BOTH_SHIFTS_TURNS_ON_CAPS_WORD)
+=======
+CAPS_WORD_ENABLE = yes
+>>>>>>> 483aee036c (Updated Keymap, Enabled Caps Word and defined BOTH_SHIFTS_TURNS_ON_CAPS_WORD)
+||||||| parent of 5a734ba134 (Added via support and reduced firmware size to fit)
+CAPS_WORD_ENABLE = yes
+=======
+CAPS_WORD_ENABLE = yes
+VIA_ENABLE = yes
+LTO_ENABLE = yes	#Recuse the size of the firmware through link time optimization
+||||||| parent of e2edbbf8b2 (Fuxed typo in comments. Disabled Space Cadet)
+LTO_ENABLE = yes	#Recuse the size of the firmware through link time optimization
+=======
+LTO_ENABLE = yes	#Reduce the size of the firmware through link time optimization
+>>>>>>> e2edbbf8b2 (Fuxed typo in comments. Disabled Space Cadet)
+#Following rules to reduce size of firmware size
+GRAVE_ESC_ENABLE = no
+MUSIC_ENABLE = no
+<<<<<<< HEAD
+>>>>>>> 5a734ba134 (Added via support and reduced firmware size to fit)
+||||||| parent of e2edbbf8b2 (Fuxed typo in comments. Disabled Space Cadet)
+=======
+SPACE_CADET_ENABLE = no
+
+>>>>>>> e2edbbf8b2 (Fuxed typo in comments. Disabled Space Cadet)
